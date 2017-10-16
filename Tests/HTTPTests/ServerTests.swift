8cd404d4e033a31d858c84bba423c7cc5bb17fc9
Carl Brown <carlb@pobox.com>
2017-10-16 12:23:29 -0500

    Cause WithoutKeepAlive test to run and pass on Linux

--- Tests/HTTPTests/ServerTests.swift
+++ Tests/HTTPTests/ServerTests.swift
@@ -295,6 +295,7 @@ class ServerTests: XCTestCase {
             request1.httpMethod = "POST"
             request1.httpBody = testString1.data(using: .utf8)
             request1.setValue("text/plain", forHTTPHeaderField: "Content-Type")
+            request1.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
 
             let dataTask1 = session.dataTask(with: request1) { (responseBody, rawResponse, error) in
                 let response = rawResponse as? HTTPURLResponse
@@ -314,7 +315,7 @@ class ServerTests: XCTestCase {
                 request2.httpMethod = "POST"
                 request2.httpBody = testString2.data(using: .utf8)
                 request2.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-                request2.setValue("close", forHTTPHeaderField: "Connection")
+                request2.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
                 let dataTask2 = session.dataTask(with: request2) { (responseBody2, rawResponse2, error2) in
                     let response2 = rawResponse2 as? HTTPURLResponse
                     XCTAssertNil(error2, "\(error2!.localizedDescription)")
@@ -333,7 +334,7 @@ class ServerTests: XCTestCase {
                     request3.httpMethod = "POST"
                     request3.httpBody = testString3.data(using: .utf8)
                     request3.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-                    request3.setValue("close", forHTTPHeaderField: "Connection")
+                    request3.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
                     let dataTask3 = session.dataTask(with: request3) { (responseBody, rawResponse, error) in
                         let response = rawResponse as? HTTPURLResponse
                         XCTAssertNil(error, "\(error!.localizedDescription)")
@@ -535,6 +536,7 @@ class ServerTests: XCTestCase {
         ("testRequestKeepAliveEchoEndToEnd", testRequestKeepAliveEchoEndToEnd),
         ("testRequestLargeEchoEndToEnd", testRequestLargeEchoEndToEnd),
         ("testExplicitCloseConnections", testExplicitCloseConnections),
+        ("testMultipleRequestWithoutKeepAliveEchoEndToEnd",testMultipleRequestWithoutKeepAliveEchoEndToEnd),
         ("testRequestLargePostHelloWorld", testRequestLargePostHelloWorld),
     ]
 }
